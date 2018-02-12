bfd77c8be3835ecbc17f5e3769f69e7ea988ae08
David Wood <david@davidtw.co>
2018-02-12 23:32:53 +0000

    Added initial (mostly untested) version of account service and improved transaction service.

---
+++ service/services/account.lua
@@ -0,0 +1,192 @@
+bus:register("account")
+
+local PREFIX = "acc-"
+local ID_KEY = "__account_id"
+
+-- We need to keep track of account IDs. If there isn't a ID already stored in Redis,
+-- then store 0.
+if not redis:get(ID_KEY) then
+    -- For now, we can only get/set tables from redis.
+    redis:set(ID_KEY, { id = 0 })
+end
+
+-- Accept or reject pending transactions.
+bus:add_event_listener("PendingTransaction", function(event_type, key, correlation, data)
+    log:debug("received " .. event_type .. " event")
+    local from_acc_key = PREFIX .. data.fromAccountId
+    local to_acc_key = PREFIX .. data.toAccountId
+    local account = redis:get(from_acc_key)
+    if account then
+        -- If the user can afford the transaction then go for it.
+        if account.balance - data.amount > 0 then
+            -- If we accept a transaction, send a accepted transaction event out.
+            bus:send("AcceptedTransaction", key, true, correlation, { transaction_id = data.id })
+
+            -- Also send the debit and credit. Ensure that these are ordered.
+            bus:send("ConfirmedCredit", to_acc_key, false, correlation, {
+                id = data.toAccountId,
+                amount = data.amount,
+                note = "Transaction from " .. data.fromAccountId .. " to " .. data.toAccountId,
+            })
+            bus:send("ConfirmedDebit", from_acc_key, false, correlation, {
+                id = data.fromAccountId,
+                amount = data.amount,
+                note = "Transaction from " .. data.fromAccountId .. " to " .. data.toAccountId,
+            })
+        -- If the user can't, don't.
+        else
+            -- If we reject a transaction, just send a rejected transaction event out.
+            bus:send("RejectedTransaction", key, true, correlation, { transaction_id = data.id })
+        end
+    else
+        log:warn("received " .. event_type .. " that is not in redis")
+    end
+end)
+
+-- Create a new account in Redis.
+function create_account(id, request_id, send_event)
+    -- Set our consistency key to be "acc-X" where X is the ID. It's useful to add a prefix
+    -- because then we can filter on the key from Redis in other operations.
+    local key = PREFIX .. id
+
+    -- Create default information for an account.
+    local account = {
+        id = id,
+        balance = 0,
+        statements = {},
+        statement_item_id = 0,
+        request_id = request_id
+    }
+
+    -- Save that data.
+    redis:set(key, account)
+
+    if send_event then
+        -- Send the account created event.
+        bus:send("AccountCreated", key, true, correlation, {
+            acc_id = id, request_id = data.request_id
+        })
+    end
+end
+
+-- Handle request for an account creation.
+bus:add_event_listener("AccountCreationRequest", function(event_type, key, correlation, data)
+    log:debug("received " .. event_type .. " event")
+    -- Get the next ID.
+    local last_id = redis:get(ID_KEY)
+    local id = last_id.id + 1
+    redis:set(ID_KEY, { id = next_id })
+
+    -- Create a new account and send the event out.
+    create_account(data.request_id, true)
+end)
+
+function handle_balance_change(event_type, key, correlation, data)
+    log:debug("received " .. event_type .. " event")
+    local account = redis:get(key)
+    if account then
+        -- Update the balance to add the amount credited. data.amount should be negative if this
+        -- is a debit.
+        account.balance = account.balance + data.amount
+        -- Insert a new statement that reflects this change.
+        table.insert(account.statements, {
+            item = account.statement_item_id + 1,
+            amount = data.amount,
+            note = data.note
+        })
+        -- Increment this account's statement item id.
+        account.statement_item_id = account.statement_item_id + 1
+        -- Save these changes.
+        redis:set(key, account)
+    else
+        log:warn("received " .. event_type .. " event that is not in redis")
+    end
+end
+
+-- Adjust the balance when a confirmed debit or credit happens.
+bus:add_event_listener("ConfirmedCredit", handle_balance_change)
+bus:add_event_listener("ConfirmedDebit", function(event_type, key, correlation, data)
+    -- The ConfirmedDebit event has a positive value so negate this so that the same function
+    -- can handle both credit and debit balance changes.
+    data.amount = -data.amount
+    handle_balance_change(event_type, key, correlation, data)
+end)
+
+function rebuild_id(previous_id)
+    -- When rebuilding, make sure we keep the ID correct.
+    id = redis:get(ID_KEY)
+    if previous_event.id > id then
+        redis:set(ID_KEY, { id = previous_event.id })
+    end
+end
+
+bus:add_rebuild_handler("AccountCreation", function(event_type, key, correlation, data)
+    -- Rebuild the ID if we can.
+    rebuild_id(data.acc_id)
+    -- Create a new account with the same request ID but without the event being created.
+    create_account(data.acc_id, data.request_id, false)
+end)
+
+bus:add_rebuild_handler("ConfirmedCredit", function(event_type, key, correlation, data)
+    -- Rebuild the ID if we can.
+    rebuild_id(data.id)
+    -- Use the same balance addition function as before - it works either way as it doesn't produce
+    -- any events.
+    handle_balance_change(event_type, key, correlation, data)
+end)
+bus:add_rebuild_handler("ConfirmedDebit", function(event_type, key, correlation, data)
+    -- Rebuild the ID if we can.
+    rebuild_id(data.id)
+    -- The ConfirmedDebit event has a positive value so negate this so that the same function
+    -- can handle both credit and debit balance changes.
+    data.amount = -data.amount
+    -- Use the same balance addition function as before - it works either way as it doesn't produce
+    -- any events.
+    handle_balance_change(event_type, key, correlation, data)
+end)
+
+function handle_receipt(status, event_type, key, correlation, data)
+    log:debug("received " .. event_type .. " receipt")
+    -- Resend the event.
+    if status == "inconsistent" then
+        bus:send(event_type, key, false, correlation, data)
+    end
+end
+
+-- Only handle the receipts for event types that this service sends out.
+bus:add_receipt_listener("AccountCreation", handle_receipt)
+bus:add_receipt_listener("AcceptedTransaction", handle_receipt)
+bus:add_receipt_listener("RejectedTransaction", handle_receipt)
+bus:add_receipt_listener("ConfirmedCredit", handle_receipt)
+bus:add_receipt_listener("ConfirmedDebit", handle_receipt)
+
+-- /account/{id} returns the balance (and other details) of the requested account.
+bus:add_route("/account/{accountId}", "GET", function(method, route, args, data)
+    log:debug("received " .. route .. " request")
+    -- Get the information we have stored about this account.
+    local data = redis:get(PREFIX .. args.id)
+
+    if data then
+        -- Return some of the data.
+        return { id = data.id, balance = data.balance }
+    else
+        -- Return an error if we don't have data.
+        return { error = "could not find account with id: " .. args.id }
+    end
+end)
+
+-- /account/{id}/statement returns a sequence of credits and debits that have happened to the
+-- account.
+bus:add_route("/account/{accountId}/statement", "GET", function(method, route, args, data)
+    log:debug("received " .. route .. " request")
+    -- Get the information we have stored about this transaction.
+    local data = redis:get(PREFIX .. args.id)
+
+    if data then
+        -- Return some of the data.
+        return { id = data.id, statements = data.statements }
+    else
+        -- Return an error if we don't have data.
+        return { error = "could not find account with id: " .. args.id }
+    end
+end)
--- service/services/transaction.lua
+++ service/services/transaction.lua
@@ -1,7 +1,7 @@
 bus:register("transaction")
 
-local TRANS_PREFIX = "trans-"
-local ID_KEY = "__current_id"
+local PREFIX = "trans-"
+local ID_KEY = "__transaction_id"
 
 -- We need to keep track of transaction IDs. If there isn't a ID already stored in Redis,
 -- then store 0.
@@ -10,32 +10,39 @@ if not redis:get(ID_KEY) then
     redis:set(ID_KEY, { id = 0 })
 end
 
-bus:add_event_listener("AcceptedTransaction", function(event_type, key, correlation, data)
-    log:debug("received accepted transaction event")
-    local transaction = redis:get(key)
-    -- Update the status of the transaction when we see it was accepted.
-    if transaction then
-        transaction.status = "accepted"
-        redis:set(key, transaction)
-    else
-        log:warn("received accepted transaction event that is not in redis")
+function handle_accepted_or_rejected(transaction_status)
+    -- This returns the function that is expected as the argument to add_event_listener but
+    -- with the correct status.
+    return function(event_type, key, correlation, data)
+        log:debug("received " .. event_type .. " event")
+        local transaction = redis:get(key)
+        -- Update the status of the transaction when we see it was accepted.
+        if transaction then
+            transaction.status = transaction_status
+            redis:set(key, transaction)
+        else
+            log:warn("received " .. event_type .. " event that is not in redis")
+        end
     end
-end)
+end
 
-bus:add_event_listener("RejectedTransaction", function(event_type, key, correlation, data)
-    log:debug("received rejected transaction event")
-    local transaction = redis:get(key)
-    -- Update the status of the transaction when we see it was rejected.
-    if transaction then
-        transaction.status = "rejected"
-        redis:set(key, transaction)
-    else
-        log:warn("received rejected transaction event that is not in redis")
+-- Handle these in a generic way.
+bus:add_event_listener("AcceptedTransaction", handle_accepted_or_rejected("accepted"))
+bus:add_event_listener("AcceptedTransaction", handle_accepted_or_rejected("rejected"))
+
+function rebuild_id(previous_id)
+    -- When rebuilding, make sure we keep the ID correct.
+    id = redis:get(ID_KEY)
+    if previous_event.id > id then
+        redis:set(ID_KEY, { id = previous_event.id })
     end
-end)
+end
 
 bus:add_rebuild_handler("PendingTransaction", function(event_type, key, correlation, data)
-    log:debug("received pending transaction rebuild")
+    log:debug("received " .. event_type .. " rebuild")
+    -- Rebuild the ID if we can.
+    rebuild_id(data.id)
+
     local transaction = redis:get(key)
     -- If this transaction is not already in redis (it could be if we got the accepted or rejected
     -- transaction first).
@@ -46,51 +53,43 @@ bus:add_rebuild_handler("PendingTransaction", function(event_type, key, correlat
     end
 end)
 
-bus:add_rebuild_handler("AcceptedTransaction", function(event_type, key, correlation, data)
-    log:debug("received accepted transaction rebuild")
-    local transaction = redis:get(key)
-    -- If this transaction is not already in redis (it could be if we got the pending or rejected
-    -- transaction first).
-    if not transaction then
-        local status = { status = "accepted" }
-        for k, v in pairs(status) do data[k] = v end
-        redis:set(key, data)
-    -- If the transaction does exist, just update the status.
-    else
-        transaction.status = "accepted"
-        redis:set(key, transaction)
-    end
-end)
+function rebuild_accepted_or_rejected(transaction_status)
+    -- This returns the function that is expected as the argument to add_rebuild_handler but
+    -- with the correct status.
+    return function(event_type, key, correlation, data)
+        log:debug("received " .. event_type .. " rebuild")
+        -- Rebuild the ID if we can.
+        rebuild_id(data.id)
 
-bus:add_rebuild_handler("RejectedTransaction", function(event_type, key, correlation, data)
-    log:debug("received rejected transaction rebuild")
-    local transaction = redis:get(key)
-    -- If this transaction is not already in redis (it could be if we got the accepted or pending
-    -- transaction first).
-    if not transaction then
-        local status = { status = "rejected" }
-        for k, v in pairs(status) do data[k] = v end
-        redis:set(key, data)
-    -- If the transaction does exist, just update the status.
-    else
-        transaction.status = "rejected"
-        redis:set(key, transaction)
+        local transaction = redis:get(key)
+        -- If this transaction is not already in redis.
+        if not transaction then
+            local status = { status = transaction_status }
+            for k, v in pairs(status) do data[k] = v end
+            redis:set(key, data)
+        -- If the transaction does exist, just update the status.
+        else
+            transaction.status = transaction_status
+            redis:set(key, transaction)
+        end
     end
-end)
+end
+
+-- Handle these in a generic way.
+bus:add_rebuild_handler("AcceptedTransaction", rebuild_accepted_or_rejected("accepted"))
+bus:add_rebuild_handler("RejectedTransaction", rebuild_accepted_or_rejected("rejected"))
 
 -- We only need a receipt listener for event types that we send.
 bus:add_receipt_listener("PendingTransaction", function(status, event_type, key, correlation, data)
-    log:debug("received pending transaction receipt")
+    log:debug("received " .. event_type .. " receipt")
     local transaction = redis:get(key)
 
     if transaction then
         -- If this transaction was inconsistent (it shouldn't be, we use implicit consistency),
         -- then resend it.
         if status == "inconsistent" then
-
             -- Resend the event.
-            bus:send("PendingTransaction", key, true, nil, data)
-
+            bus:send(event_type, key, false, correlation, data)
         -- If the transaction has not already been accepted/rejected then
         -- we update the status to 'pending'.
         elseif transaction.status == "created" then
@@ -98,14 +97,13 @@ bus:add_receipt_listener("PendingTransaction", function(status, event_type, key,
             redis:set(key, transaction)
         end
     else
-        log:warn("received receipt for transaction that is not in redis")
+        log:warn("received receipt for " .. event_type .. " that is not in redis")
     end
 end)
 
 -- /createTransaction produces an `PendingTransaction` event.
 bus:add_route("/createTransaction", "POST", function(method, route, args, data)
-    log:debug("received create transaction request")
-
+    log:debug("received " .. route .. " request")
     -- Get the next ID.
     local last_id = redis:get(ID_KEY)
     local id = last_id.id + 1
@@ -123,7 +121,7 @@ bus:add_route("/createTransaction", "POST", function(method, route, args, data)
 
     -- Set our consistency key to be "trans-X" where X is the ID. It's useful to add a prefix
     -- because then we can filter on the key from Redis in other operations.
-    local key = TRANS_PREFIX .. id
+    local key = PREFIX .. id
 
     -- Store information about this new transaction.
     -- We create a new table with the extra information that we will combine with `event_data`
@@ -146,13 +144,9 @@ end)
 
 -- /transactions returns the status of a transaction.
 bus:add_route("/transaction/{id}", "GET", function(method, route, args, data)
-    log:debug("received transaction request")
-
-    -- Find the ID of the transaction that will have been persisted.
-    local key = TRANS_PREFIX .. args.id
-
+    log:debug("received " .. route .. " request")
     -- Get the information we have stored about this transaction.
-    local data = redis:get(key)
+    local data = redis:get(PREFIX .. args.id)
 
     if data then
         -- Return the data we have.
