c342ce6961277501b73b3f9ed4ded7969c76fbdd
David Wood <david@davidtw.co>
2018-02-27 22:12:43 +0000

    Modified query/rebuild to be a single message.

--- common/src/schemas/mod.rs
+++ common/src/schemas/mod.rs
@@ -2,6 +2,7 @@ pub mod consistency;
 pub mod event;
 pub mod new_event;
 pub mod query;
+pub mod rebuild;
 pub mod receipt;
 pub mod register;
 pub mod registration;
@@ -14,6 +15,7 @@ pub use self::consistency::{
 pub use self::event::Event;
 pub use self::new_event::{NewEvent, NewEvents};
 pub use self::query::Query;
+pub use self::rebuild::Rebuild;
 pub use self::receipt::{Receipt, Receipts};
 pub use self::register::Register;
 pub use self::registration::Registration;
